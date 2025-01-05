0a116880681f743648bc5e65fa66960f1e975025
Maks-Nata <mixcim20121984@gmail.com>
2025-01-05 20:01:34 +0200

    style

--- src/components/myComponent.tsx
+++ src/components/myComponent.tsx
@@ -1,8 +1,9 @@
 import {FC} from "react";
+import './myComponent.css'
 
 type MyComponentPropType={text:string}
 const MyComponent:FC<MyComponentPropType>=({text}:MyComponentPropType)=> {
-    return <><div>{text}</div> </>
+    return <><div className={'target'}>{text}</div> </>
 }
 export default MyComponent;
 
