273e23c6a5a5e16226019a7329c073a908a1c90b
Maks-Nata <mixcim20121984@gmail.com>
2025-01-05 21:07:49 +0200

    tailwind

--- src/components/myComponent.tsx
+++ src/components/myComponent.tsx
@@ -1,9 +1,9 @@
 import {FC} from "react";
-import './myComponent.css'
+
 
 type MyComponentPropType={text:string}
 const MyComponent:FC<MyComponentPropType>=({text}:MyComponentPropType)=> {
-    return <><div className={'target'}>{text}</div> </>
+    return <><div className={'text-3xl font-bold underline'}>{text}</div> </>
 }
 export default MyComponent;
 
