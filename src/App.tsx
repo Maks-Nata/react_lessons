ca6796dd35b95bcc6456289ae395178547a84ae1
Maks-Nata <mixcim20121984@gmail.com>
2025-01-10 12:45:33 +0200

    deleted data from the file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import {useState} from "react";
+
 
 
 const App = () => {
-    const[counter,setCounter]=useState<number>(0);
+
 
   return (
       <div>
-<h2>{counter}</h2>
-          <button onClick={()=>{setCounter(prevState=>{return ++prevState})}}>increment</button>
-          <button onClick={()=>{setCounter(prevState=>{return --prevState})}}>decrement</button>
+
       </div>
   );
 };
